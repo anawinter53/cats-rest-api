610e024f70bce6b27888cc74b07f4bd090f0cd8d
“anawinter53” <anatolia.winter@yahoo.com>
2023-03-14 21:21:45 +0000

    add cat data

---
+++ cats.js
@@ -0,0 +1,92 @@
+const cats = [
+{
+    "id": 1,
+    "breed": "Maine Coon",
+    "name": "Luna"
+},
+{
+    "id": 2,
+    "breed": "Maine Coon",
+    "name": "Nala"
+},
+{
+    "id": 3,
+    "breed": "Maine Coon",
+    "name": "Oliver"
+},
+{
+    "id": 4,
+    "breed": "Ragdoll",
+    "name": "Leo"
+},
+{
+    "id": 5,
+    "breed": "Ragdoll",
+    "name": "Simba"
+},
+{
+    "id": 6,
+    "breed": "Ragdoll",
+    "name": "Luna"
+},
+{
+    "id": 7,
+    "breed": "Siamese",
+    "name": "Milo"
+},
+{
+    "id": 8,
+    "breed": "Siamese",
+    "name": "Tigger"
+},
+{
+    "id": 9,
+    "breed": "Siamese",
+    "name": "Max"
+},
+{
+    "id": 10,
+    "breed": "Sphynx",
+    "name": "Lola"
+},
+{
+    "id": 11,
+    "breed": "Sphynx",
+    "name": "Bella"
+},
+{
+    "id": 12,
+    "breed": "Sphynx",
+    "name": "Oreo"
+},
+{
+    "id": 13,
+    "breed": "Scottish Fold",
+    "name": "Whiskey"
+},
+{
+    "id": 14,
+    "breed": "Scottish Fold",
+    "name": "Cappucino"
+},
+{
+    "id": 15,
+    "breed": "Scottish Fold",
+    "name": "Irn Bru"
+},
+{
+    "id": 16,
+    "breed": "Japanese Bobtail",
+    "name": "Miso"
+},
+{
+    "id": 17,
+    "breed": "Japanese Bobtail",
+    "name": "Wasabi"
+},
+{
+    "id": 18,
+    "breed": "Japanese Bobtail",
+    "name": "Mochi"
+}
+]
